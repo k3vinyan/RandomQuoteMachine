454b6581db751cb3e190ce2653cac0e5a78ee27b
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2020-04-12 17:06:44 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /.js$/,
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /.css$/,
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/assets/template.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 9001
+    },
+    mode: 'development'
+}
+
+module.exports = config
